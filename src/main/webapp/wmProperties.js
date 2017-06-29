533b76b2b3183043d4d3831a08533f726b13b940
root <root@61949b3d2f95>
2017-06-29 17:59:56 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_QvpHqEueLQ",
+  "homePage" : "Main",
+  "name" : "Auto_QvpHqEueLQ",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
